10b00ad622ce32ab7bb688262fd2e941b63f4bd8
kimjisu <kimjisu@DESKTOP-84ALIDK>
2022-09-09 01:22:36 +0900

    2022-09-09 오전01:21 오류해결못함

--- src/main/java/com/fintech/mvc/financialterm/model/service/FinancialtermServiceImpl.java
+++ src/main/java/com/fintech/mvc/financialterm/model/service/FinancialtermServiceImpl.java
@@ -1,6 +1,7 @@
 package com.fintech.mvc.financialterm.model.service;
 
 import org.mybatis.spring.SqlSessionTemplate;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
@@ -8,24 +9,27 @@ import org.springframework.transaction.annotation.Transactional;
 
 import com.fintech.mvc.financialterm.model.dao.FinancialtermDao;
 import com.fintech.mvc.financialterm.model.dao.FinancialtermDaoImpl;
+import com.fintech.mvc.financialterm.model.mapper.FinancialtermMapper;
 import com.fintech.mvc.financialterm.model.vo.Financialterm;
 
 @Service
 public class FinancialtermServiceImpl implements FinancialtermService{
 	
 	
-	private FinancialtermDao dao = new FinancialtermDaoImpl();
-		
+//	private FinancialtermDao dao = new FinancialtermDaoImpl();
 	@Autowired
-	@Qualifier("sqlSession") 
-	private SqlSessionTemplate session;
+	private FinancialtermMapper mapper;
+	
+//	@Autowired
+//	@Qualifier("sqlSession") 
+//	private SqlSessionTemplate session;
 	
 	@Override
 	@Transactional(rollbackFor = Exception.class)
 	public int saveFinancialterm(Financialterm ft) {
 		int result = 0;
 		
-		result = dao.insertFinancialterm(session, ft);
+		result = mapper.insertFinancialterm(ft);
 		System.out.println(result);
 		
 		return result;
