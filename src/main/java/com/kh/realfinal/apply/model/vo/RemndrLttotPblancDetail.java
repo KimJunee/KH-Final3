60f4ee99447a0d6cd74123267d628f2a7768c43e
Yenny <pureaa94@gmail.com>
2022-09-22 13:30:43 +0900

    220922 안예은 - 청약 파싱 코드 수정

--- src/main/java/com/kh/realfinal/apply/model/vo/RemndrLttotPblancDetail.java
+++ src/main/java/com/kh/realfinal/apply/model/vo/RemndrLttotPblancDetail.java
@@ -8,14 +8,14 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class RemndrLttotPblancDetail {
-	private String	house_manage_no	        ; // 주택관리번호
+	private int		house_manage_no	        ; // 주택관리번호
 	private String	pblanc_no	            ; // 공고번호
-	private String	house_nm	            ; // 주택명
-	private String 	house_secd	            ; // 주택구분코드
-	private String	house_secd_nm	        ; // 주택구분코드명
-	private String	hssply_zip	            ; // 공급위치 우편번호
+	private String 	house_nm				; // 주택명
+	private int 	house_secd				; // 주택구분코드
+	private String 	house_secd_nm			; // 주택구분코드명
+	private int		hssply_zip	            ; // 공급위치 우편번호
 	private String	hssply_adres	        ; // 주소
-	private String	tot_suply_hshldco	    ; // 공급규모
+	private int		tot_suply_hshldco	    ; // 공급규모
 	private String	rcrit_pblanc_de	        ; // 모집공고일
 	private String	subscrpt_rcept_bgnd     ; // 청약접수시작일
 	private String	subscrpt_rcept_endd     ; // 청약접수종료일
