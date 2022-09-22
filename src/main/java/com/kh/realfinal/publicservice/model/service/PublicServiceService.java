bd59ef52fc6aa5b16dbea683bc0ae65be09ac0c8
user <user@host.docker.internal>
2022-09-22 15:17:49 +0900

    api 삭제 - 김태임

--- src/main/java/com/kh/realfinal/publicservice/model/service/PublicServiceService.java
+++
@@ -1,7 +0,0 @@
-package com.kh.realfinal.publicservice.model.service;
-
-import com.kh.realfinal.publicservice.model.vo.PublicService;
-
-public interface PublicServiceService {
-	int savePublicService(PublicService ps);
-}
