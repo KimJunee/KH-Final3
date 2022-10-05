731f8f54cd44a0f9cce729ddec06a188e10bf3df
JuneeKim <jrgetit0323@gmail.com>
2022-10-05 10:19:05 +0900

    회원가입, 로그인, 마이페이지 내글보기 jsp 완료, 로고 및 사진 추가, jsp 경로 추가 - 김주니

--- src/main/java/com/kh/realfinal/HomeController.java
+++ src/main/java/com/kh/realfinal/HomeController.java
@@ -1,8 +1,6 @@
 package com.kh.realfinal;
 
-
 import java.util.Locale;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
@@ -10,23 +8,26 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-/**
- * Handles requests for the application home page.
- */
 @Controller
 public class HomeController {
 	
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
 	
-	/**
-	 * Simply selects the home view to render by returning its name.
-	 */
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {
-		//test();
-		return "mypageBoardList";
-		//return "signIn";
-		//return "communityMain";
+		return "home";
 	}
 	
+	/**
+	 * http://localhost/realfinal/hi?url=community/communityMain -> 경로
+	 * @param locale
+	 * @param model
+	 * @param url
+	 * @return
+	 */
+	@RequestMapping(value = "/hi", method = RequestMethod.GET)
+	public String AnyWhere(Locale locale, Model model, String url) {
+		System.out.println("Where am i >> " + url);
+		return url;
+	}
 }
