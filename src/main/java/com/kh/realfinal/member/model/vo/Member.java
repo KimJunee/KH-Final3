52789112ef50beb4439f6b79ccc92a5c643fdc7f
kimjisu <kimjisu@DESKTOP-84ALIDK>
2022-10-07 00:49:03 +0900

    2022-10-07 MEMBER 변수 수정 및 추가 김지수

--- src/main/java/com/kh/realfinal/member/model/vo/Member.java
+++ src/main/java/com/kh/realfinal/member/model/vo/Member.java
@@ -8,11 +8,12 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class Member {
-	private int no;	
-	private String id;	
-	private String password;
-	private String name;
-	private String phone;	
-	private String email;
-	private String status;
+	private int user_no;	
+	private String user_id;	
+	private String user_name;
+	private String user_nickname;
+	private String user_phone;	
+	private String user_password;
+	private String user_email;
+	private String user_status;
 }
