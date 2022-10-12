bf9e04d8f098f368126964c3b9756724308926e7
SOPHIA JEON <pinkychuchu@gmail.com>
2022-10-12 22:26:21 +0900

    국회의원 사진 파싱 완료 / 전소피아 / 22.10.12

--- src/main/java/com/kh/realfinal/politicsMnaList/api/MnaListAPI.java
+++ src/main/java/com/kh/realfinal/politicsMnaList/api/MnaListAPI.java
@@ -15,11 +15,6 @@ import org.w3c.dom.NodeList;
 
 import com.kh.realfinal.politicsMnaList.model.vo.MnaList;
 
-//국회 국회사무처_의원 정보(사진)
-//http://apis.data.go.kr/9710000/NationalAssemblyInfoService/getMemberCurrStateList?
-//ServiceKey=tWbk8PS%2BF%2BjjXa2fQXIqiPRWUGRnEAzT%2F8bGnTZ0SWG1V7XoO1VEs4%2Ba5IlbouHxPoF9pGG%2FAgs57DaAjxYU%2BQ%3D%3D	
- 
-
 public class MnaListAPI {
 	
 	public static String key = "=tWbk8PS%2BF%2BjjXa2fQXIqiPRWUGRnEAzT%2F8bGnTZ0SWG1V7XoO1VEs4%2Ba5IlbouHxPoF9pGG%2FAgs57DaAjxYU%2BQ%3D%3D";
@@ -104,43 +99,4 @@ public class MnaListAPI {
 		return list;
 	}
 }
-				
-				
-				
- 
-	/* Java 1.8 샘플 코드 */
-//	    public static void main(String[] args) throws IOException {
-//	        StringBuilder urlBuilder = new StringBuilder("http://apis.data.go.kr/9710000/NationalAssemblyInfoService/getMemberCurrStateList"); /*URL*/
-//	        urlBuilder.append("?" + URLEncoder.encode("serviceKey","UTF-8") + "=tWbk8PS%2BF%2BjjXa2fQXIqiPRWUGRnEAzT%2F8bGnTZ0SWG1V7XoO1VEs4%2Ba5IlbouHxPoF9pGG%2FAgs57DaAjxYU%2BQ%3D%3D"); /*Service Key*/
-//	        urlBuilder.append("&" + URLEncoder.encode("numOfRows","UTF-8") + "=" + URLEncoder.encode("10", "UTF-8")); /*한 페이지 결과 수*/
-//	        urlBuilder.append("&" + URLEncoder.encode("pageNo","UTF-8") + "=" + URLEncoder.encode("1", "UTF-8")); /*페이지 번호*/
-//	        URL url = new URL(urlBuilder.toString());
-//	        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-//	        conn.setRequestMethod("GET");
-//	        conn.setRequestProperty("Content-type", "application/json");
-//	        System.out.println("Response code: " + conn.getResponseCode());
-//	        BufferedReader rd;
-//	        if(conn.getResponseCode() >= 200 && conn.getResponseCode() <= 300) {
-//	            rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-//	        } else {
-//	            rd = new BufferedReader(new InputStreamReader(conn.getErrorStream()));
-//	        }
-//	        StringBuilder sb = new StringBuilder();
-//	        String line;
-//	        while ((line = rd.readLine()) != null) {
-//	            sb.append(line);
-//	        }
-//	        rd.close();
-//	        conn.disconnect();
-//	        System.out.println(sb.toString());
-//	    }
-	    
-//	    public static List<ListMna> callListMnaByXML {
-//	    	
-//	    	List<ListMna> list = new ArrayList<ListMna>();
-//	    	
-//	    	
-//	    	
-//	    }
-	    
-	    
+	 
