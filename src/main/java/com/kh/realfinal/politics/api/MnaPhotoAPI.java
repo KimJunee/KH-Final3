0bae62357adf08f5673ddee7f0fea63e1bf3b2c1
SOPHIA JEON <pinkychuchu@gmail.com>
2022-10-13 15:55:20 +0900

    정치 국회의원 프로필사진 파싱 완료 / 전소피아 / 22.10.13

--- src/main/java/com/kh/realfinal/politics/api/MnaPhotoAPI.java
+++ src/main/java/com/kh/realfinal/politics/api/MnaPhotoAPI.java
@@ -13,22 +13,31 @@ import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 
-import com.kh.realfinal.politics.model.vo.MnaList;
+import com.kh.realfinal.politics.model.vo.MnaPhoto;
 
-public class MnaListAPI {
+//국회 국회사무처_의원 정보(국회의원 프로필 사진)
+
+public class MnaPhotoAPI {
 	
-	public static String key = "=tWbk8PS%2BF%2BjjXa2fQXIqiPRWUGRnEAzT%2F8bGnTZ0SWG1V7XoO1VEs4%2Ba5IlbouHxPoF9pGG%2FAgs57DaAjxYU%2BQ%3D%3D";
+	public static String key = "tWbk8PS%2BF%2BjjXa2fQXIqiPRWUGRnEAzT%2F8bGnTZ0SWG1V7XoO1VEs4%2Ba5IlbouHxPoF9pGG%2FAgs57DaAjxYU%2BQ%3D%3D";
 	public static String ListMna_XML_URL = "http://apis.data.go.kr/9710000/NationalAssemblyInfoService/getMemberCurrStateList";
 
 	public static void main(String[] args) {
-		MnaListAPI.callListMnaByXML();
+		List<MnaPhoto> list = MnaPhotoAPI.callListMnaByXML();
+//		for(MnaPhoto photo : list) {
+//			if(photo.getMnaName().equals("배현진")) {
+//				System.out.println("===========================");
+//				System.out.println("배현진을 찾았습니다!!");
+//				System.out.println(photo);
+//			}
+//		}
 	}
 	
 	
-	private static List<MnaList> callListMnaByXML() {
-		List<MnaList> list = new ArrayList<MnaList>();
+	private static List<MnaPhoto> callListMnaByXML() {
+		List<MnaPhoto> list = new ArrayList<MnaPhoto>();
 		int pageNo = 1;
-		int numOfRows = 299;
+		int numOfRows = 500;
 		
 		while (true) {
 			
@@ -36,9 +45,9 @@ public class MnaListAPI {
 			System.out.println("pageNo : " + pageNo);
 			StringBuilder urlBuilder = new StringBuilder(ListMna_XML_URL);
 			
-			urlBuilder.append("?" + "serviceKey" + key);
-			urlBuilder.append("&" + "pageNo=" + pageNo);
-			urlBuilder.append("&" + "numOfRows" + numOfRows);
+			urlBuilder.append("?" + "ServiceKey=" + key);
+			urlBuilder.append("&" + "numOfRows=" + numOfRows);
+//			urlBuilder.append("&" + "pageNo=" + pageNo);
 			System.out.println(urlBuilder);
 			
 			try {
@@ -84,19 +93,20 @@ public class MnaListAPI {
 							mnaPhoto = eElement.getElementsByTagName("jpgLink").item(0).getTextContent();
 						}
 						
-						MnaList lm = new MnaList(mnaName, mnaPhoto);
+						MnaPhoto lm = new MnaPhoto(mnaName, mnaPhoto);
 						list.add(lm);
 					}
-					
-					
 				}
+				pageNo++;
+				if(pageNo == 2) {
+					break;
+				}
+				
 			} catch (Exception e) {
 				e.printStackTrace();
-			
 			}
-			
 		}
 		return list;
 	}
 }
-	 
+				
--- src/main/java/com/kh/realfinal/politics/model/vo/MnaPhoto.java
+++ src/main/java/com/kh/realfinal/politics/model/vo/MnaPhoto.java
@@ -1,7 +1,5 @@
 package com.kh.realfinal.politics.model.vo;
 
-
-
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -9,9 +7,8 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-	public class MnaList {
-		private String mnaName; // 국회의원 이름
-		private String mnaPhoto; // 국회의원 프로필 사진
-		
- 
+public class MnaPhoto {
+	private String mnaName; // 국회의원 이름
+	private String mnaPhoto; // 국회의원 프로필 사진
+
 }
