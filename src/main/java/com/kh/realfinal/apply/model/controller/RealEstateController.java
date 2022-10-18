dfbcaa8a82d22e58555d4cb46b72bc6ef2a6fd2c
MHLEE12 <coolmh12@gmail.com>
2022-10-18 10:59:32 +0900

    부동산청약 controller 수정 - 이미향

--- src/main/java/com/kh/realfinal/apply/model/controller/RealEstateController.java
+++ src/main/java/com/kh/realfinal/apply/model/controller/RealEstateController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import com.kh.realfinal.apply.model.service.RealEstateService;
 import com.kh.realfinal.apply.model.vo.RemndrLttotPblancDetail;
 import com.kh.realfinal.common.util.PageInfo;
+import com.kh.realfinal.apply.model.service.ApplyInfoApiService;
 
 @Controller
 public class RealEstateController {
@@ -19,6 +20,9 @@ public class RealEstateController {
 	
 	@Autowired
 	private RealEstateService remndrService;
+	
+	@Autowired
+	private ApplyInfoApiService ApplyService;
 	
 	
 	@RequestMapping("/RealEstate/main")
