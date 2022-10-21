f3239de1f7e9a0318fbf62db79af0c117f0e403b
lsi55 <lsi55@host.docker.internal>
2022-10-21 09:07:29 +0900

    오피니언 컨트롤러 - 이상일

--- src/main/java/com/kh/realfinal/news/controller/OpinionController.java
+++ src/main/java/com/kh/realfinal/news/controller/OpinionController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class OpinionController {
 
-	@RequestMapping("/opinion")
+	@RequestMapping("/news/opinion")
 	String newsSearch() {
 		return "/news/opinion";
 	}
