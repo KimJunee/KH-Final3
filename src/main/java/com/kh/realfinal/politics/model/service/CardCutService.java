858a1f5c9d5faf760394c9a6b6aea5f7f7377383
SOPHIA JEON <pinkychuchu@gmail.com>
2022-10-23 18:34:45 +0900

    누락 파일 업로드

--- src/main/java/com/kh/realfinal/politics/model/service/CardCutService.java
+++ src/main/java/com/kh/realfinal/politics/model/service/CardCutService.java
@@ -7,16 +7,14 @@ import com.kh.realfinal.common.util.PageInfo;
 import com.kh.realfinal.politics.model.vo.CardCut;
 import com.kh.realfinal.politics.model.vo.CardCutReply;
 
-public interface CardCutService {
+public interface CardCutService { //
+	int saveCardCut(CardCut card);
 	
-	public int saveCardCut(CardCut card);
-	public List<CardCut> getCardCutList(PageInfo pageInfo, Map<String, String> param);
-	public List<CardCut> getCardCutList(PageInfo pageInfo);
+	List<CardCut> getCardCutList(PageInfo pageInfo, Map<String, String> param);
+	int getCardCutCount(Map<String, String> param);
 	
-	int deleteReply(int no);
-
+	int deleteReply(int no);  
 	int saveCcReply(CardCutReply reply);
-	public int getCardCutList();
-	public int getCardCutCount();
+	CardCut getCardCutContent(int cardCutNo);
 	
 }
--- src/main/java/com/kh/realfinal/politics/model/service/LawProposedService.java
+++ src/main/java/com/kh/realfinal/politics/model/service/LawProposedService.java
@@ -1,19 +1,15 @@
 package com.kh.realfinal.politics.model.service;
 
 import java.util.List;
+import java.util.Map;
 
 import com.kh.realfinal.common.util.PageInfo;
 import com.kh.realfinal.politics.model.vo.LawProposed;
 
 public interface LawProposedService  {
-	
 	public int saveLawProposed(LawProposed proposed);
-
-	public List<LawProposed> getlawProposed(PageInfo pageInfolaw, String name);
-
+	public List<LawProposed> getlawProposed(PageInfo pageInfolaw, Map<String, String> param);
+	public int getLawCount(Map<String, String> param);
 	public LawProposed getLawProposed(int lawNo);
-
-
-	public int getLawCount(int page);
  
 }
