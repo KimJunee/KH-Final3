6dc7a37d4e9b2c3e2debb4aa38cf82c87182c2af
JuneeKim <jrgetit0323@gmail.com>
2022-10-24 17:19:56 +0900

    오피니언 1개 출력 성공 임시 커밋 - 김주니

--- src/main/java/com/kh/realfinal/opinion/model/service/OponionServiceImpl.java
+++ src/main/java/com/kh/realfinal/opinion/model/service/OponionServiceImpl.java
@@ -1,9 +1,12 @@
 package com.kh.realfinal.opinion.model.service;
 
+import java.util.List;
+import java.util.Map;
+import org.apache.ibatis.session.RowBounds;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
+import com.kh.realfinal.common.util.PageInfo;
 import com.kh.realfinal.opinion.model.mapper.OpinionMapper;
 import com.kh.realfinal.opinion.model.vo.Opinion;
 
@@ -13,6 +16,7 @@ public class OponionServiceImpl implements OpinionService{
 	@Autowired
 	private OpinionMapper mapper;
 
+	// 오피니언 DB INSERT
 	@Override
 	@Transactional(rollbackFor = Exception.class)
 	public int saveOpinion(Opinion opinion) {
@@ -20,4 +24,22 @@ public class OponionServiceImpl implements OpinionService{
 		return result;
 	}
 
+	// 오피니언 목록
+	@Override
+	public List<Opinion> getOpinionList(PageInfo pageInfo, Map<String, String> param) {
+		int offset = (pageInfo.getCurrentPage() - 1) * pageInfo.getListLimit();
+		RowBounds rowBounds = new RowBounds(offset, pageInfo.getListLimit());
+
+		return mapper.selectOpinion(rowBounds, param);
+	}
+
+	@Override
+	public int getOpinionCount(Map<String, String> param) {
+		return mapper.selectOpinionCount(param);
+	}
+	
+	public Opinion getOpinionOne(int opinionNo) {
+		Opinion opinion = mapper.selectOpinionOne(opinionNo);
+		return opinion;
+	}
 }
--- src/main/java/com/kh/realfinal/opinion/model/vo/Opinion.java
+++ src/main/java/com/kh/realfinal/opinion/model/vo/Opinion.java
@@ -4,9 +4,13 @@ import java.util.Date;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Data
+@Getter
+@Setter
 @NoArgsConstructor
 @AllArgsConstructor
 public class Opinion {
